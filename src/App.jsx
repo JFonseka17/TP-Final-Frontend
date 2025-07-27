970c4b067549a9488696e1a7c837e97a166bcfcd
Julian fonseca <fonseka17.jf@gmail.com>
2025-07-27 15:56:04 -0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,81 @@
-import React,{useState} from "react"
+import React, { useState } from "react"
+import { Route, Routes, Outlet } from 'react-router'
 import ChatScreen from './Screens/ChatScreen/ChatScreen'
-import { Route, Routes } from 'react-router'
 import ContactScreen from "./Screens/ContactScreen/ContactScreen"
+import "./App.css"
+
+const Layout = () => {
+	return (
+		<div className="A">
+			<header className="aa"></header>
+
+			{/* Columna 1: Lista de contactos (SIEMPRE visible) */}
+			<div className="ab">
+				<ContactScreen />
+			</div>
+
+			{/* Columna 2: Área dinámica (aquí entra ChatScreen) */}
+			<div className="ac">
+				<Outlet /> {/* Esto se reemplazará por la ruta hija */}
+			</div>
+		</div>
+	);
+};
 
 const App = () => {
+	return (
+		<Routes>
+			{/* Ruta principal con Layout fijo */}
+			<Route path="/" element={<Layout />}>
+				{/* Ruta hija: se renderiza en el <Outlet /> del Layout */}
+				<Route
+					path="contact/:contact_id/messages"
+					element={<ChatScreen />}
+				/>
+			</Route>
+		</Routes>
+	);
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+/* const App = () => {
 
 	return (
-			<div>
-				<Routes>
-					<Route
-						path="/"
-						element={<ContactScreen/>}
-					/>
-					<Route
+			<div className="A">
+				<header className="aa">
+				</header>
+				<div className="ab">
+					<Routes>
+						<Route
+							path="/"
+							element={<ContactScreen/>}
+						/>
+					</Routes>
+				</div>
+				<div className="ac">
+					<img src="Fondo TP.jpg" alt="" />
+					<Routes>
+						<Route
 						path="/contact/:contact_id/messages"
 						element={<ChatScreen/>}
-					/>
-				</Routes>
+						/>
+					</Routes>
+				</div>
+				
 			</div>
 	)
 }
 
-export default App
+export default App */
